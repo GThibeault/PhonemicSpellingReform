8cb6d9fca81be8b238f5b3393944d79ba50a0e05
Gabriel Thibeault <gabriel.eric.thibeault@gmail.com>
2022-06-11 17:59:24 +0200

    Export/Import from clipboard

---
+++ English.Web/Logic/JsonSerializerOptionsBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+
+namespace English.Web.Logic;
+
+public class JsonSerializerOptionsBuilder
+{
+    public JsonSerializerOptions BuildOptions() => new()
+    {
+        Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+    };
+}
--- English.Web/Program.cs
+++ English.Web/Program.cs
@@ -11,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddTransient(sp => new SpellingMapper());
 builder.Services.AddTransient(sp => new PhonemizationFetcher(sp.GetService<HttpClient>()!));
+builder.Services.AddTransient(sp => new JsonSerializerOptionsBuilder());
 builder.Services.AddSingleton(sp => new PhonemeMappingModel());
 builder.Services.AddSingleton(sp => new InputTextModel());
 
