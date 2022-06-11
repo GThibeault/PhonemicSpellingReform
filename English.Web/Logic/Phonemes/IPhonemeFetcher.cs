9f724495b96554ad5a4319ed33f28ca556b4d5de
Gabriel Thibeault <gabriel.eric.thibeault@gmail.com>
2022-06-11 16:03:14 +0200

    Phoneme mappings as jsons

--- English.Web/Logic/Phonemes/IPhonemeFetcher.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Linq;
-
-namespace English.Web.Logic.Phonemes;
-
-public interface IPhonemeFetcher
-{
-    public IEnumerable<String> GetPhonemes() => GetPhonemeDict().Keys.ToList();
-    public Dictionary<String, String> GetPhonemeDict() => GetPhonemes().ToDictionary(phoneme => phoneme, phoneme => phoneme);
-}
