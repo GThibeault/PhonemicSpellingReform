d4f2da54bbce454f6b4437580f80581f40c0c5be
Gabriel Thibeault <gabriel.eric.thibeault@gmail.com>
2022-07-17 22:34:22 +0200

    Working local azure function app implementation of phonemizer

--- English.Phonemizer/phonemize/__init__.py
+++ English.Phonemizer/phonemize/__init__.py
@@ -1,38 +1,10 @@
-#from flask import request
-#from flask_cors import CORS, cross_origin
-#from . import app
-#from phonemizer import phonemize
-
-#cors = CORS(app)
-#app.config["CORS_HEADERS"] = "Content-Type"
-
-
-#@app.route("/phonemize", methods=["POST"])
-#@cross_origin()
-#def phonemize_handler():
-#    request_data = request.get_json()
-#    text, language = request_data["text"], request_data["language"] 
-    
-#    phonemized = phonemize(
-#        text,
-#        language= 'en-us' if language is None else language,
-#        backend="espeak",
-#        preserve_empty_lines=True,
-#        preserve_punctuation=True,
-#        with_stress=True,
-#    )
-
-#    return {"phonemized": phonemized}
-
 from phonemizer import phonemize
 
-import logging
+import json
 import azure.functions as func
 
-def main(request: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-    
-    request_data = request.get_json()
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    request_data = req.get_json()
     text, language = request_data["text"], request_data["language"]
     
     phonemized = phonemize(
@@ -45,5 +17,6 @@ def main(request: func.HttpRequest) -> func.HttpResponse:
     )
 
     response_body = {"phonemized": phonemized}
+    json_body = json.dumps(response_body)
 
-    return func.HttpResponse(body=response_body)
+    return func.HttpResponse(body=json_body)
